e8f6d53fbc92f554db1f48b995d0ec93176a6088
dmoralgo <dmoralgo@EMEAL.NTTDATA.COM>
2024-06-25 13:05:09 -0600

    Se asigno el @Data se vieron los metodos que nos genera

--- src/main/java/com/lombok/Person.java
+++ src/main/java/com/lombok/Person.java
@@ -5,16 +5,10 @@ import lombok.experimental.FieldDefaults;
 
 import java.time.LocalDate;
 
-@Setter // por defecto son públicos
-@Getter// (value = AccessLevel.PROTECTED) // se les puede asignar un nivel de acceso
-@AllArgsConstructor // crea un constructor con argumentos //(staticName = "of") //para crear un método estático que devuelva una instancia de la clase con los atributos requeridos
-@NoArgsConstructor // crea un constructor vacío //(access = AccessLevel.PUBLIC) // se les puede asignar un nivel de acceso
-@FieldDefaults(level = AccessLevel.PRIVATE /*makeFinal = true // hace que sean Final*/) // se les puede asignar un nivel de acceso a los atributos
-//@ToString(exclude = {"address", "city", "state", "country", "zipCode"})//se crea el toString // se puden excluir atributos
-//@ToString(includeFieldNames = false) // muestra solo los valores de los atributos
-@ToString(of = {"id", "name", "email", "phone", "birthDate"}) // muestra solo los valores de los atributos
-//@EqualsAndHashCode(/* exclude = {"name"} nos excluye los paraentros que queramos*/ /*of = {"id", "name", "email", "phone", "birthDate"} nos ibluye los parametros que queremos*/) // se les puede asignar los atributos a comparar// no crea el equeals y el hashcode
-@EqualsAndHashCode(doNotUseGetters = true) // no usa los getters perono es muy comun
+@Data // genera los métodos getter, setter, equals, hashcode, toString
+@AllArgsConstructor // genera un constructor con todos los atributos
+@NoArgsConstructor // genera un constructor vacío
+@FieldDefaults(level = AccessLevel.PRIVATE) // se puede asignar a nivel de clase porque el @Data no lo asigna
 public class Person {
 
     //@Setter // se pueden asignar a nivel de atributo
