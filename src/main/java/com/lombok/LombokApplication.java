dbe5bff2c429e65526f5f842057dc1041eb8e3a9
dmoralgo <dmoralgo@EMEAL.NTTDATA.COM>
2024-06-25 13:25:04 -0600

    Se asigno el @Log y @Slf4j para trabajar con log

--- src/main/java/com/lombok/LombokApplication.java
+++ src/main/java/com/lombok/LombokApplication.java
@@ -1,11 +1,16 @@
 package com.lombok;
 
+import lombok.extern.java.Log;
+
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+//@Log // no es tan recomendado usarlo de esta forma
+@Slf4j // es mejor usarlo de esta forma
 public class LombokApplication {
 
 	public static void main(String[] args) {
@@ -16,22 +21,9 @@ public class LombokApplication {
 	public CommandLineRunner commandLineRunner() {
 		return args -> {
 			Person person = new Person( 1L, "John Doe", "que@emeal.com", "1234567890", "123 Main St", "Springfield", "IL", "USA", "62701", null);
-			//si se usan los métodos setter
-			/*person.setId(1L);
-			person.setName("John Doe");
-			person.setEmail("qew@emea.com");
-			person.setPhone("1234567890");
-			person.setAddress("123 Main St");
-			person.setCity("Springfield");
-			person.setState("IL");
-			person.setCountry("USA");
-			person.setZipCode("62701");
-			person.setBirthDate(null);*/
-
-			//si se usa el método estático of
-			//Person persna = Person.of(1L, "John Doe", "que@emeal.com", "1234567890", "123 Main St", "Springfield", "IL", "USA", "62701", null);
-
 			System.out.println(person);
+			log.info(person.toString());
+			//maneja info, warning, error, etc
 
 		};
 
